c40d2db16528d11999c8f27ae612f280e5f227c6
Eryn Wells <eryn@erynwells.me>
2012-02-24 21:54:52 -0800

    Double quote all paths in setup script
    Linux (Cygwin) doesn't like spaces in path names so setup.sh fails utterly on
    Windows if you have a username with spaces in it. This change double quotes all
    the ln and touch commands so space-addled paths are handled properly.

--- setup.sh
+++ setup.sh
@@ -2,32 +2,32 @@
 
 dfdir=$(cd "$(dirname "$0")" && pwd)
 
-ln -s $dfdir/profile $HOME/.profile
-ln -s $dfdir/rc $HOME/.rc
+ln -s "$dfdir/profile" "$HOME/.profile"
+ln -s "$dfdir/rc" "$HOME/.rc"
 
-ln -s $dfdir/zshrc $HOME/.zshrc
-ln -s $dfdir/zsh $HOME/.zsh
-ln -s $dfdir/zprofile $HOME/.zprofile
-ln -s $dfdir/zshenv $HOME/.zshenv
+ln -s "$dfdir/zshrc" "$HOME/.zshrc"
+ln -s "$dfdir/zsh" "$HOME/.zsh"
+ln -s "$dfdir/zprofile" "$HOME/.zprofile"
+ln -s "$dfdir/zshenv" "$HOME/.zshenv"
 
-ln -s $dfdir/vimrc $HOME/.vimrc
-ln -s $dfdir/vim $HOME/.vim
+ln -s "$dfdir/vimrc" "$HOME/.vimrc"
+ln -s "$dfdir/vim" "$HOME/.vim"
 
-ln -s $dfdir/indent.pro $HOME/.indent.pro
-ln -s $dfdir/screenrc $HOME/.screenrc
-ln -s $dfdir/ledgerrc $HOME/.ledgerrc
-ln -s $dfdir/toprc $HOME/.toprc
+ln -s "$dfdir/indent.pro" "$HOME/.indent.pro"
+ln -s "$dfdir/screenrc" "$HOME/.screenrc"
+ln -s "$dfdir/ledgerrc" "$HOME/.ledgerrc"
+ln -s "$dfdir/toprc" "$HOME/.toprc"
 
-ln -s $dfdir/gitconfig $HOME/.gitconfig
+ln -s "$dfdir/gitconfig" "$HOME/.gitconfig"
 
-ln -s $dfdir/irssi $HOME/.irssi
+ln -s "$dfdir/irssi" "$HOME/.irssi"
 
-[ `uname -s` = "Linux" ] && ln -s $dfdir/Xdefaults $HOME/.Xdefaults
+[ `uname -s` = "Linux" ] && ln -s "$dfdir/Xdefaults" "$HOME/.Xdefaults"
 
-touch $HOME/.hushlogin
+touch "$HOME/.hushlogin"
 
 # Initialized submodules
-cd $dfdir
+cd "$dfdir"
 git submodule init
 git submodule update
 
