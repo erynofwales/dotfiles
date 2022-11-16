833c4c8a6fe3472dcd74c01567a0172a4448f79c
Eryn Wells <eryn@erynwells.me>
2022-11-16 08:58:07 -0800

    [vim] Add ensureMetadataDirectoriesExist() and call it
    This function creates backup, undo, and swap directories if they do not already exist

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -5,3 +5,20 @@ vim.cmd [[ source ~/.vimrc.common ]]
 
 require 'configuration'
 require 'keys'
+
+function ensureMetadataDirectoriesExist()
+    paths = {
+        vim.opt.backupdir:get(),
+        vim.opt.directory:get(),
+        vim.opt.undodir:get()
+    }
+
+    for _, opt in ipairs(paths) do
+        local first_path = opt[1]
+        if string.find(first_path, "//$") then
+            os.execute("mkdir -p " .. first_path)
+        end
+    end
+end
+
+ensureMetadataDirectoriesExist()
