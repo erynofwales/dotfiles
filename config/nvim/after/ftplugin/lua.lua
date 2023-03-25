970ed67788686ab2df1ad0564e5d7fedd9aa15f2
Eryn Wells <eryn@erynwells.me>
2023-03-25 08:09:09 -0700

    [nvim] Comment about why resolving symlinks is necessary in config/nvim/after/ftplugin/lua.lua

--- config/nvim/after/ftplugin/lua.lua
+++ config/nvim/after/ftplugin/lua.lua
@@ -4,9 +4,9 @@ local expand = vim.fn.expand
 local resolve = vim.fn.resolve
 local stdpath = vim.fn.stdpath
 
+-- My dotfiles are usually symlinks. Resolve them so the path comparison makes sense.
 local configPath = resolve(stdpath("config"))
 local fullPath = resolve(expand("%:p"))
-
 if string.find(fullPath, configPath) == 1 then
     vim.opt_local.path = {
         ".",
