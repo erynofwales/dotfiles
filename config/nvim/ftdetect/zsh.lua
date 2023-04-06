cd98e5d1d62593b3f1818d67ef3346b530aa54ec
Eryn Wells <eryn@erynwells.me>
2023-04-06 08:25:25 -0700

    [vim] Add a group to the ZSH path auto commands

--- config/nvim/ftdetect/zsh.lua
+++ config/nvim/ftdetect/zsh.lua
@@ -1,6 +1,8 @@
 -- Eryn Wells <eryn@erynwells.me>
 
+local dotfilesZshGroup = vim.api.nvim_create_augroup("DotfilesZSH", {clear = true})
 vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
     pattern = {"*/zsh/*"},
+    group = dotfilesZshGroup,
     command = "setfiletype zsh",
 })
