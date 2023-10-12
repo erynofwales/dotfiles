74e66401f8dda11b80d47af152ef8187b058f555
Eryn Wells <eryn@erynwells.me>
2023-10-12 10:45:55 -0700

    [neovim] Remove some stuff from Python config

--- config/nvim/after/ftplugin/python.lua
+++ config/nvim/after/ftplugin/python.lua
@@ -1,12 +1,2 @@
 -- Eryn Wells <eryn@erynwells.me>
 
-local safariToolsPath = vim.env.sfi .. "/Tools/Scripts"
-
-vim.opt_local.path = {
-    ".",
-    "",
-    safariToolsPath,
-    safariToolsPath .. "/safaripy",
-}
-
-
