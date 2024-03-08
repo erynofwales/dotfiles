49fe168cc987569e1811e1ef2255e2a8cee8becb
Eryn Wells <eryn@erynwells.me>
2024-03-08 14:31:29 -0800

    [vim] Make sure the gutter is always visible after loading gitgutter

--- config/nvim/after/plugin/gitgutter.lua
+++ config/nvim/after/plugin/gitgutter.lua
@@ -1,5 +1,8 @@
 -- Eryn Wells <eryn@erynwells.me>
 
+-- Ensure there's always a gutter column so there's no stutter when changes cause it to appear.
+vim.wo.signcolumn = "yes"
+
 vim.cmd [[
     highlight GitGutterAdd ctermfg=Green ctermbg=0
     highlight GitGutterChange ctermfg=DarkBlue ctermbg=0
