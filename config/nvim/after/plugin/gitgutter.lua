5e5c8f83c89711f48dd34f5daebefba7eb8ce0db
Eryn Wells <eryn@erynwells.me>
2024-03-08 08:23:18 -0800

    [vim] Customize gitgutter colors a little bit

---
+++ config/nvim/after/plugin/gitgutter.lua
@@ -0,0 +1,7 @@
+-- Eryn Wells <eryn@erynwells.me>
+
+vim.cmd [[
+    highlight GitGutterAdd ctermfg=Green ctermbg=0
+    highlight GitGutterChange ctermfg=DarkBlue ctermbg=0
+    highlight GitGutterDelete ctermfg=Red ctermbg=0
+]]
