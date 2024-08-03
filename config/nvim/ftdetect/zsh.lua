3110e2ff765533454ca272b098d5afae0f96409f
Eryn Wells <eryn@erynwells.me>
2024-08-02 18:24:30 -0700

    [vim] Remove the autocmd group from the Zsh autocommands in its ftdetect script
    Apparently autocmds in ftdetect scripts are automatically grouped, so they don't
    need an explicit group.

--- config/nvim/ftdetect/zsh.lua
+++ config/nvim/ftdetect/zsh.lua
@@ -1,8 +1,6 @@
 -- Eryn Wells <eryn@erynwells.me>
 
-local dotfilesZshGroup = vim.api.nvim_create_augroup("DotfilesZSH", {clear = true})
 vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
     pattern = {"*/zsh/*"},
-    group = dotfilesZshGroup,
     command = "setfiletype zsh",
 })
