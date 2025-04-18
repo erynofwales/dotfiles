2fea369110b102610e5d2dd03a2e28058a0f4e03
Eryn Wells <eryn@erynwells.me>
2025-04-18 09:55:11 -0700

    [nvim] Move setting the GUI font to init_gui() from _init_neovide()
    Make a font family list that includes Berkeley Mono, SF Mono, and Courier New.

--- config/nvim/lua/gui.lua
+++ config/nvim/lua/gui.lua
@@ -9,12 +9,12 @@ local function _init_neovide()
     vim.g.neovide_position_animation_length = 0
     vim.g.neovide_scroll_animation_length = 0
 
-    vim.o.guifont = "InputMonoCondensed:h16"
 
     vim.cmd [[ colorscheme lunaperche ]]
 end
 
 function init_gui()
+    vim.o.guifont = "Berkeley Mono,Input Mono Condensed,SF Mono,Courier New:h18"
     _init_neovide()
 end
 
